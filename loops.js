bf602af0eec29e86282a88f8d2649ea5cffb7491
Kathleen Turner <kate@Kathleens-MacBook-Pro.local>
2018-06-14 12:36:04 -0600

    loops finished

---
+++ loops.js
@@ -0,0 +1,34 @@
+// for (let i=0; i <= 99; i++) {
+	// console.log(`${i}`);
+// }
+
+// for (let i=99; i >= 0; i--) {
+	// console.log(i);
+// if(i % 2 === 0) {
+// console.log(i);
+// }
+// }
+
+// for (let i = 98; i >= 0; i--) {
+// 	if(i % 2 === 0) {
+// console.log(i);
+// }
+// }
+
+// for (let i = 49; i < 73; i++) {
+// 	console.log(i);
+// }
+
+// for (let i = 81; i > 25; i--) {
+// 	console.log(i);
+// }
+
+for (let i = 3; i < 91; i++) {
+	if(i % 3 == 0) {
+		console.log(i);
+	}
+}
+
+
+
+	
